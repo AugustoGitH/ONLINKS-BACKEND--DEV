5d77c29652b950b4e424d01a7170c0fc1a74b8cb
Augusto Caetano Westphal <100123518+AugustoGitH@users.noreply.github.com>
2024-05-12 21:05:17 -0300

    Delete dist directory

--- dist/@types/express.d.js
+++
@@ -1,3 +0,0 @@
-// declare namespace Express {
-//   export interface Request {}
-// }
--- dist/models/LinkPage/enums.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ThemePage = void 0;
-var ThemePage;
-(function (ThemePage) {
-    ThemePage["DEFAULT"] = "default";
-})(ThemePage = exports.ThemePage || (exports.ThemePage = {}));
